2ad834b7f26e38ff4e082529f9ac01b604ee9178
Rashid Saleem <mrashidcit@gmail.com>
2017-10-09 15:35:01 +0500

    AsyncTask is completed

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,54 +1,177 @@
 package com.example.myfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myfirstapp.dummy.DummyContent;
+import com.example.myfirstapp.utilities.NetworkUtils;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
-    private TextView mweatherTextView;
+    private EditText mSearchBoxEditText;
+
+    private TextView mUrlDisplayTextView;
+
+    private TextView mSearchResultsTextView;
+
+    // reference to error Message Textview
+    private TextView mErrorMessageDisplay;
+
+    // reference to the ProgressBar
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mweatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
 
-        // Append each String from the fake wather data array to the Textview
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
-        for (String dummyWeatherDay : dummyWeatherData){
-            mweatherTextView.append(dummyWeatherDay + "\n\n\n");
-        }
+        // error Textview
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
+        // ProgressBar reference
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
 
 
     }
 
+    // Show JsonData and hide the errors
+    private void showJsonDataView() {
+        // First, make sure the error is invisible
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
 
+        // Then, make sure the JSON data is visible
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+
+    }
+
+    //
+    private void showErrorMessage() {
+
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+//        return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search){
+//            Context context = MainActivity.this;
+//            String textToShow   = "Search cliked";
+//            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+
+            makeGithubSearchQuery();
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    } // end onOptionsItemSelected()
+
+    private void makeGithubSearchQuery() {
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+
+        // Call getResponseFromHttpUrl and display the results in mSearchResultsTextView
+        // Surround the call to getResponseFromHttpUrl with
+
+//        String githubSearchResults = null;
+//        try {
+//            githubSearchResults = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
+//            mSearchBoxEditText.setText(githubSearchResults);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        new GithubQueryTask().execute(githubSearchUrl);
+
+
+    } // end makeGithubSearchQuery()
+
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String > {
+
+
+        //
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+
+
+        // to perform the query. Return the result.
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
+            try {
+
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+            return githubSearchResults;
+        } // end doInBackground()
+
+        // to display the result in the TextView
+        protected void onPostExecute(String githubSearchResults){
+
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+
+            if(githubSearchResults != null && !githubSearchResults.equals("")){
+                mSearchResultsTextView.setText(githubSearchResults);
+                showJsonDataView();
+            } else {
+//                mSearchResultsTextView.setText("Result not Found");
+                showErrorMessage();
+//                String result = githubSearchResults;
+                Log.println(Log.INFO, "Response-Result", "Rashid Saleem");
+
+            }
+
+        }
+
+    } // end GitHubQueryTask()
 
 
 }
