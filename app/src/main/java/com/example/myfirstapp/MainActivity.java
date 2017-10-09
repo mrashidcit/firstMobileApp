608e93a2ad593df07f1fba9eee0928bb3740014d
Rashid Saleem <mrashidcit@gmail.com>
2017-10-09 10:28:33 +0500

    add files to change the branch to master

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myfirstapp.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity
+              implements HeadlinesFragment.OnListFragmentInteractionListener  {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_article);
+
+        // Check the the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being resored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+
+            if(savedInstanceState != null){
+                return ;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+
+            // In case this activity was started with spacial from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments((getIntent().getExtras()));
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        } // end if checking fragment_container
+
     }
+
+
 
     // Called when the user taps the Send button
     public void sendMessage(View view){
@@ -22,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
