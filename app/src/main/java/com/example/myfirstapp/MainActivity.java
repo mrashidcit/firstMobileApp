9fe21e127d79bdd655f1216d17ec227432cd4274
Rashid Saleem <mrashidcit@gmail.com>
2017-10-12 12:33:04 +0500

    Project with fresh code

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -9,56 +9,16 @@ import android.widget.EditText;
 
 import com.example.myfirstapp.dummy.DummyContent;
 
-public class MainActivity extends AppCompatActivity
-              implements HeadlinesFragment.OnListFragmentInteractionListener  {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_article);
+        setContentView(R.layout.activity_main);
 
-        // Check the the activity is using the layout version with
-        // the fragment_container FrameLayout
-        if (findViewById(R.id.fragment_container) != null) {
 
-            // However, if we're being resored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-
-            if(savedInstanceState != null){
-                return ;
-            }
-
-            // Create a new Fragment to be placed in the activity layout
-            HeadlinesFragment firstFragment = new HeadlinesFragment();
-
-            // In case this activity was started with spacial from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments((getIntent().getExtras()));
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment).commit();
-        } // end if checking fragment_container
 
     }
 
 
-
-    // Called when the user taps the Send button
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-
-
-    @Override
-    public void onListFragmentInteraction(DummyContent.DummyItem item) {
-
-    }
 }
